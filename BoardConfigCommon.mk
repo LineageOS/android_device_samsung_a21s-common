b8f947d8ef14bfc5255fa3689bd21b68a6f60b82
SamarV-121 <samarvispute121@pm.me>
2021-09-12 12:40:30 +0530

    a21s-common: Import libinit for variant handling
     * Based on https://github.com/SamarV-121/android_device_samsung_universal7904-common/tree/c43956c1ee977a4fcf4825696fe6dfaa2cb22c81/init
     * Added support for overriding device,name props
    
    Change-Id: Ia19abcbd0402a80dfa2869718544f14bc11decc4
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,6 +107,9 @@ TARGET_KERNEL_SOURCE := kernel/samsung/exynos850
 TARGET_KERNEL_CLANG_VERSION := r416183b
 TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/kernel/$(HOST_PREBUILT_TAG)/clang-$(TARGET_KERNEL_CLANG_VERSION)
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_exynos3830
+
 # A/B
 AB_OTA_UPDATER := false
 
