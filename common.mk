253a944ac7d950bd8027fa23432576359fde3890
Thomas Turner <thomas_turner@talktalk.net>
2025-06-08 12:35:50 +0100

    a21s-common: Drop touch HAL
    Change-Id: If4b8db6d27dd66e6c55ed794fbf12c6692ae8a27

--- common.mk
+++ common.mk
@@ -281,7 +281,7 @@ PRODUCT_COPY_FILES += \
 
 # Touch HAL
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
+    vendor.lineage.touch-service.samsung
 
 # USB
 PRODUCT_PACKAGES += \
