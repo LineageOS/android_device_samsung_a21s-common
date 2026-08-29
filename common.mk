ff0a6f1d01870c5ec663217554a244bcf4f57abc
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 10:19:42 +0300

    a21s-common: Drop UFFD flag
    See: Ibb2c5e9191744f6e15f6bced337ba7d0ed9681ca
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61

--- common.mk
+++ common.mk
@@ -144,7 +144,6 @@ $(call soong_config_set_bool,libion,legacy_impl,true)
 # Kernel
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
-PRODUCT_ENABLE_UFFD_GC := true
 
 # Keymaster
 PRODUCT_PACKAGES += \
